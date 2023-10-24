d5fd3046c8fd4bad24295dc584505a59ae1aabab
João Pedro Trevisan <joao.treviisan@outlook.com>
2023-10-23 22:49:40 -0300

    atualizar imagens

--- Exercicios/Ex01/script.js
+++ Exercicios/Ex01/script.js
@@ -3,15 +3,17 @@ function carregar() {
     var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours() 
+
+    hora = 9
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 5 && hora <= 11 ) {
-        img.src = 'fotomanha.jpg'
+        img.src = 'fotomanha.png'
         document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora <= 18 ) {
-        img.src = 'fototarde.jpg'
+        img.src = 'fototarde.png'
         document.body.style.background = '#b9846f'
     } else {
-        img.src = 'fotonoite.jpg'
+        img.src = 'fotonoite.png'
         document.body.style.background = '#515154'
     }
 
