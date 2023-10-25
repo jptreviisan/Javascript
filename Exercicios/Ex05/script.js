a582545470508663f1d6576278f87357e8c29c76
João Pedro Trevisan <145926289+jptreviisan@users.noreply.github.com>
2023-10-25 16:24:25 -0300

    Criei Ex05

---
+++ Exercicios/Ex05/script.js
@@ -0,0 +1,8 @@
+var num = document.getElementById('fnum')
+var lista = document.getElementById('flist')
+var res = document.getElementById('res')
+var valores = []
+
+function adicionar() {
+    
+}
